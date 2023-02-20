14d6f16ca6f3ca6d4df65fd0ee232662a643e9b9
Vitali Bedik <vitalya.bedik@gmail.com>
2023-02-20 22:52:28 +0300

    feat: create basic folder structure

---
+++ src/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+import DetailRecipe from './pages/DetailRecipe';
+import Recipes from './pages/Recipes';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Recipes />
+      <DetailRecipe />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/App/pages/Recipes/Recipes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Cards from './components/Cards';
+
+const Recipes = () => {
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
+};
+
+export default Recipes;
