d378f2cd4ac6fe38852fa5a7cac63b34b494127a
Vitali Bedik <vitalya.bedik@gmail.com>
2023-02-25 00:43:05 +0300

    feat: fetch DetailRecipe from api

--- src/App/pages/DetailRecipe/DetailRecipe.tsx
+++ src/App/pages/DetailRecipe/DetailRecipe.tsx
@@ -5,49 +5,78 @@ import ReturnIcon from '@components/ReturnIcon';
 import likeImg from '@images/like.svg';
 import menuPhoto from '@images/menuPhoto.png';
 import timeIcon from '@images/time.svg';
+import { transformRecipeApiData } from '@utils/transformRecipeApiData';
 
 import RecipeInfoItem from './components/RecipeInfoItem';
 import styles from './DetailRecipe.module.scss';
+import spoonacularApi from '../../../api/spoonacular-api';
+import { extendedIngredients, RecipeDetailItemTypes } from '../../../types/RecipeDetailItemTypes';
 
 const DetailRecipe = () => {
+  const [recipeDetail, setRecipeDetail] = React.useState<RecipeDetailItemTypes | null>(null);
+
+  React.useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const { data } = await spoonacularApi.getDetailRecipe(recipeId);
+
+      setRecipeDetail({
+        id: data.id,
+        image: data.image,
+        title: data.title,
+        likes: data.aggregateLikes,
+        readyInMinutes: data.readyInMinutes,
+        ingredients: data.extendedIngredients.map((item: extendedIngredients) => item.name),
+        summary: data.summary,
+        instructions: data.instructions,
+        dishTypes: data.dishTypes,
+      });
+    };
+
+    const recipeId = 1;
+    fetchData();
+  }, []);
+
+  // eslint-disable-next-line no-console
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.detailRecipe}>
-        <Button className={styles.button__return}>
-          <ReturnIcon />
-        </Button>
-        <img className={styles.detailRecipe__img} src={menuPhoto} alt="menu__img" />
-        <div className={styles['detailRecipe__wrapper-mask']}>
-          <div className={styles['detailRecipe__wrapper-content']}>
-            <div className={styles.detailRecipe__line}></div>
-            <h1 className={styles.detailRecipe__title}>Chicken Burger Promo Pack</h1>
-            <div className={styles['detailRecipe__recipe-info']}>
-              <RecipeInfoItem img={timeIcon} value={4.8} infoType="minutes" />
-              <RecipeInfoItem img={likeImg} value={4.8} infoType="Rating" />
+        {recipeDetail && (
+          <>
+            <Button className={styles.button__return}>
+              <ReturnIcon />
+            </Button>
+
+            <img className={styles.detailRecipe__img} src={recipeDetail.image} alt="menu__img" />
+            <div className={styles['detailRecipe__wrapper-mask']}>
+              <div className={styles['detailRecipe__wrapper-content']}>
+                <div className={styles.detailRecipe__line}></div>
+                <h1 className={styles.detailRecipe__title}>{recipeDetail.title}</h1>
+                <div className={styles['detailRecipe__recipe-info']}>
+                  <RecipeInfoItem img={timeIcon} value={recipeDetail.readyInMinutes} infoType="minutes" />
+                  <RecipeInfoItem img={likeImg} value={recipeDetail.likes} infoType="Rating" />
+                </div>
+                <div className={styles.detailRecipe__recipe}>
+                  <div
+                    className={styles.detailRecipe__summary}
+                    dangerouslySetInnerHTML={{ __html: recipeDetail.summary }}
+                  ></div>
+                  <div className={styles.detailRecipe__ingredients}>
+                    <ul>
+                      {recipeDetail.ingredients.map((product: extendedIngredients, index: number) => {
+                        return <li key={`${product.name}-${index}`}>{product.name}</li>;
+                      })}
+                    </ul>
+                  </div>
+                  <div
+                    className={styles.detailRecipe__instructions}
+                    dangerouslySetInnerHTML={{ __html: recipeDetail.instructions }}
+                  ></div>
+                </div>
+              </div>
             </div>
-            <div className={styles.detailRecipe__recipe}>
-              <div className={styles.detailRecipe__summary}>
-                Nulla occaecat velit laborum exercitation ullamco. Elit labore eu aute elit nostrud culpa velit
-                excepteur deserunt sunt. Velit non est cillum consequat cupidatat ex Lorem laboris labore aliqua ad duis
-                eu laborum. Chicken Tomatoa Lettuse Nulla occaecat velit laborum exercitation ullamco. Elit labore eu
-                aute elit nostrud culpa velit excepteur deserunt sunt.
-              </div>
-              <div className={styles.detailRecipe__ingredients}>
-                <ul>
-                  <li>Chicken</li>
-                  <li>Tomatoa</li>
-                  <li>Lettuse</li>
-                </ul>
-              </div>
-              <div className={styles.detailRecipe__instructions}>
-                Nulla occaecat velit laborum exercitation ullamco. Elit labore eu aute elit nostrud culpa velit
-                excepteur deserunt sunt. Velit non est cillum consequat cupidatat ex Lorem laboris labore aliqua ad duis
-                eu laborum. Chicken Tomatoa Lettuse Nulla occaecat velit laborum exercitation ullamco. Elit labore eu
-                aute elit nostrud culpa velit excepteur deserunt sunt.
-              </div>
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
